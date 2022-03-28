d0318009230423fe132f767cea002c4dfa9e6b01
1853989 <1853989@01-19765.pusd.dom>
2022-03-28 10:47:28 -0700

    3/28

--- TetrisProject/src/Block.java
+++ TetrisProject/src/Block.java
@@ -5,7 +5,6 @@ public class Block {
 	private static ArrayList<Block> placedBlocks= new ArrayList<Block>();
 	private static ArrayList<Block> moveBlocks = new ArrayList<Block>();
 	private int color;
-	private boolean moveable;
 	private Point coords;
 	
 	public Block(int color, int x, int y) {
@@ -14,10 +13,28 @@ public class Block {
 		moveBlocks.add(this);
 	}
 	
-	public void placeBlocks() {
+	public static void placeBlocks() {
 		for(int i = moveBlocks.size() - 1; i >= moveBlocks.size(); i--) {
 			placedBlocks.add(moveBlocks.get(i));
 			moveBlocks.remove(i);
+		}
+	}
+	
+	public static void moveDown() {
+		for(Block block : moveBlocks) {
+			block.changeY((int) block.getXY().getY() - 1);
+		}
+	}
+	//Left is false, right is true
+	public static void moveSide(boolean direction) {
+		if(direction) {
+			for(Block block : moveBlocks) {
+				block.changeX((int) block.getXY().getX() + 1);
+			}
+			return;
+		}
+		for(Block block : moveBlocks) {
+			block.changeX((int) block.getXY().getX() - 1);
 		}
 	}
 	
@@ -28,4 +45,13 @@ public class Block {
 	public int color() {
 		return color;
 	}
+	
+	public void changeX(int newX) {
+		coords.setLocation(newX, coords.getY());
+	}
+	
+	public void changeY(int newY) {
+		coords.setLocation(coords.getX(), newY);
+	}
+	
 }
